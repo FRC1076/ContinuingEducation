3048b188d0d36377479a25b8d9ec6bd0fe6ba7ac
Juno Ryan <juno@Junos-MacBook-Air.local>
2019-01-11 20:20:38 -0500

    documentation string

--- permutation/permute.py
+++ permutation/permute.py
@@ -1,4 +1,7 @@
 def permutor(text, permutation):
+    """
+    The permutor function has two arguements. It takes the text and scrambles it according to what's inputted into the permutation arguement.
+    """
     scrambled = ""
     for x in permutation:
         scrambled = scrambled + text[x]
