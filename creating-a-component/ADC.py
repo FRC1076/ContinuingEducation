013634ea49201e457770ac34afe0d90f66f9b496
Matthew Jones <mcolinj@mac.com>
2021-11-21 23:22:02 -0500

    Add wrap implementation

---
+++ creating-a-component/ADC.py
@@ -0,0 +1,129 @@
+import smbus
+import time
+class Adc:
+    """
+    Usage:
+            adc = Adc()
+
+            value = adc.recvADC(PORT_NUM)
+
+     where PORT_NUM is the port number of the ADC that is
+     doing the measurement.
+
+     Notice that the Adc class wraps the smbus class.   This can
+     act as another example of wrapping another class with a higher
+     layer of service.
+    """
+    def __init__(self):
+        # Get I2C bus
+        self.bus = smbus.SMBus(1)
+        
+        # I2C address of the device
+        self.ADDRESS            = 0x48
+        
+        # PCF8591 Command
+        # See the data sheet for this chip at:
+        #     https://www.nxp.com/docs/en/data-sheet/PCF8591.pdf
+        # Notice can also convert digital to analog signals
+        # that is why there is a write method.
+        self.PCF8591_CMD                        =0x40  #Command
+        
+        # ADS7830 Command
+        # See the data sheet for this chip at:
+        #     https://www.ti.com/product/ADS7830
+        self.ADS7830_CMD                        = 0x84 # Single-Ended Inputs
+
+        """
+        Note that the Freenove was built using two different
+        models of ADC chip.    This check determines which chip
+        is being used and remembers the name so that appropriate
+        variation of the access to the ADC can be used.
+        """
+        for i in range(3):
+            aa=self.bus.read_byte_data(self.ADDRESS,0xf4)
+            if aa < 150:
+                self.Index="PCF8591"
+            else:
+                self.Index="ADS7830" 
+    def analogReadPCF8591(self,chn):#PCF8591 read ADC value,chn:0,1,2,3
+        """
+        Read 9 times, sort the results and return the median.
+        This helps smooth out the readings, maybe increasing accuracy.
+        """
+        value=[0,0,0,0,0,0,0,0,0]
+        for i in range(9):
+            value[i] = self.bus.read_byte_data(self.ADDRESS,self.PCF8591_CMD+chn)
+        value=sorted(value)
+        return value[4]   
+        
+    def analogWritePCF8591(self,value):#PCF8591 write DAC value
+        self.bus.write_byte_data(self.ADDRESS,cmd,value)
+        
+    def recvPCF8591(self,channel):#PCF8591 write DAC value
+        """
+        Read until two successive values are equal.   This will miss
+        any short spikes.
+        """
+        while(1):
+            value1 = self.analogReadPCF8591(channel)   #read the ADC value of channel 0,1,2,
+            value2 = self.analogReadPCF8591(channel)
+            if value1==value2:
+                break;
+        voltage = value1 / 256.0 * 3.3  #calculate the voltage value
+        voltage = round(voltage,2)
+        return voltage
+    def recvADS7830(self,channel):
+        """Select the Command data from the given provided value above"""
+        COMMAND_SET = self.ADS7830_CMD | ((((channel<<2)|(channel>>1))&0x07)<<4)
+        self.bus.write_byte(self.ADDRESS,COMMAND_SET)
+        while(1):
+            value1 = self.bus.read_byte(self.ADDRESS)
+            value2 = self.bus.read_byte(self.ADDRESS)
+            if value1==value2:
+                break;
+        """
+        The ADC actually returns an 8 bit number (0-255).
+        This code scales it to a value between 0 and 3.3   (the voltage)
+        """
+        voltage = value1 / 255.0 * 3.3  #calculate the voltage value
+        voltage = round(voltage,2)
+        return voltage
+        
+    def recvADC(self,channel):
+        """
+        Depending on which chip is on this board, read voltage value from specified
+        port (channel) of the ADC chip and return it.
+        """
+        if self.Index=="PCF8591":
+            data=self.recvPCF8591(channel)
+        elif self.Index=="ADS7830":
+            data=self.recvADS7830(channel)
+        return data
+    # This function is not used in the test code (and there is no
+    # matching (open statement for the sbus (i2c bus))
+    def i2cClose(self):
+        self.bus.close()
+
+def loop():
+    adc=Adc()
+    while True:
+        Left_IDR=adc.recvADC(0)
+        print (Left_IDR)
+        Right_IDR=adc.recvADC(1)
+        print (Right_IDR)
+        Power=adc.recvADC(2)*3
+        print (Power)
+        Left_FLUX=adc.recvADC(3)
+        print (Left_FLUX)
+        Right_FLUX=adc.recvADC(4)
+        time.sleep(1)
+        print ('----')
+def destroy():
+    pass
+# Main program logic follows:
+if __name__ == '__main__':
+    print ('Program is starting ... ')
+    try:
+        loop()
+    except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, the child program destroy() will be  executed.
+        destroy()
---
+++ creating-a-component/FluxMeter-wrap.py
@@ -0,0 +1,78 @@
+from ADC import *
+
+class FluxMeter:
+    """
+    The FluxMeter wraps the Acme fm2319 to provide a simple
+    interface that is more like what robot.py needs to use.    Recall
+    that the FluxMeter is a sensor that measures etherial flux density
+    on a scale of 0 to 1.0, but the fm2319 flux meter chip actually
+    measures the flux on a scale from 0 to 5.0
+
+    Usage:
+        in robotInit, create the FluxMeter attached to the appropriate pin or port
+
+                self.flux_left = FluxMeter(FluxMeter.LEFT_PORT)
+
+        And in the main bodies of auton and teleop, you can use it to read
+        ether flux density.
+
+                lf = self.flux_left.getFlux()
+                rf = self.flux_right.getFlux()
+    """
+
+    """
+    The guiding principles when you wrap one service to create a new interface are:
+
+         1. initialize the underlying service and save a handle to it
+         2. save any other state during __init__ that are required to get
+            the correct information from the underlying service
+
+    Take a look at the ADC.py in this directory.    Some additional comments have
+    been added to it to help you understand how it can be used.     Note that you
+    can mostly figure out the minimum that you need to know by just looking at how
+    it is being tested.
+    """
+    LEFT_PORT = 3
+    RIGHT_PORT = 4
+
+    def __init__(self, port_number):
+        """
+        Remember the port number for the adc receive
+        Get a handle on the ADC to do the receive
+        """
+        self.port_number = port_number
+        self.adc = Adc()
+
+    def getFlux(self):
+        """
+        Use the wrapped data to get to the underlying adc
+        """
+        return self.adc.recvADC(self.port_number)
+
+#
+#  This "Dunder" (pythonista term derived from the double-underscores)
+#  ensures the test code only runs when the file is run.
+#         e.g.    python3 FluxMeter.py
+#  If the file is imported, this stuff does not get executed.
+#  So run it directly to test it out, and import it to use
+#  the library.
+#
+if __name__ == "__main__":
+
+    import time
+
+    # robotInit
+    fm_left = FluxMeter(FluxMeter.LEFT_PORT)
+    fm_right = FluxMeter(FluxMeter.RIGHT_PORT)
+
+    #
+    # quasi periodic test (test 10 times with 1 second between)
+    # This allows the tester to wave around the flux generator and
+    # observe the changes
+    for _ in range(10):
+        print("Left :", fm_left.getFlux())
+        print("Right :", fm_right.getFlux())
+        time.sleep(1)
+
+
+
